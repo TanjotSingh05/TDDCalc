68111b3a4b8dd584af0172542b2417ea4f815929
Tanjot Singh <sitanjot@sheridancollege.ca>
2026-02-05 16:59:58 -0500

    first

---
+++ src/main/java/com/mycompany/tddcalc/TDDCalc.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tddcalc;
+
+/**
+ *
+ * @author tanjotsingh
+ */
+public class TDDCalc {
+
+    public double divide(double a, double b){
+        double result;
+        result = a/b;
+        return result;
+    }
+}
